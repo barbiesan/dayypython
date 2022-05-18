81a8fe27fda5ecb0383d60e629c881fd2217fed1
barbiesan <barbarasolre@gmail.com>
2022-05-18 08:23:28 -0700

    first commit

---
+++ geometry.py
@@ -0,0 +1,9 @@
+from math import pi, sqrt
+def area_of_circle(radius):
+    return pi * (radius ** 2)
+
+print(area_of_circle(10))
+
+def get_hypo(side_a, side_b):
+    return sqrt((side_a **2) + (side_b **2)) 
+
